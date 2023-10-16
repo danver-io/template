d301c44aa614738b48881fe73c2062054bbb9932
Dan <0921pig@gmail.com>
2023-10-16 21:25:04 +0900

    add i18n

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,10 +1,12 @@
-'use client'
 import Button from '@danver-io/danver-design-system/dist/component/button/Button'
+import useTranslation from 'next-translate/useTranslation'
 
 export default function Home() {
+    const {t, lang} = useTranslation('common')
+
     return (
         <div className={'p-[20px]'}>
-            <Button label={'text'} variant={'danger'} />
+            <Button label={t('title')} variant={'danger'} />
         </div>
     )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextTranslate = require('next-translate-plugin')
+const config = nextTranslate({})
+config.i18n = undefined
 
-module.exports = nextConfig
+module.exports = config
