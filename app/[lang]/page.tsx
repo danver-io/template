4e549ca394a5386408c1fcd8116c1e2678a81401
Dan <0921pig@gmail.com>
2023-10-17 22:10:51 +0900

    language detection middleware

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,5 +1,6 @@
+'use client'
 import useTranslation from 'next-translate/useTranslation'
-import Button from '@danver-io/danver-design-system/dist/button/Button'
+import Button from '@danver-io/danver-design-system/dist/component/button/Button'
 
 export default function Home() {
     const {t, lang} = useTranslation('common')
@@ -7,7 +8,7 @@ export default function Home() {
     return (
         <div className={'p-[20px]'}>
             <p className={'text-title.large'}>제목</p>
-            <Button label={t('title')} variant={'danger'} />
+            <Button label={t('title')} variant={'primary'} isLoading={true} />
         </div>
     )
 }
---
+++ lib/dds/index.tsx
@@ -0,0 +1,3 @@
+'use client'
+
+export {Input, Button, ResponsiveHeader} from '@danver-io/danver-design-system/dist/components'
